a31d7e2e04b3994fe25b810597a349197dfa7f81
wangxq <shmimy-w@163.com>
2011-12-06 03:02:19 +0800

    Initial Commit

---
+++ PricingXML/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  PricingXML
+//
+//  Created by wang xuanqiang on 12/6/11.
+//  Copyright (c) 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
